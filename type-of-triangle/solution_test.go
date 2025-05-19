009b2a8b18b51db97bc82c56ebf423ae009dcf70
Igor Sapozhnikov <i.d.sapozhnikov@tbank.ru>
2025-05-19 16:49:01 +0400

    add solution to the daily task
    https://leetcode.com/problems/type-of-triangle/

---
+++ type-of-triangle/solution_test.go
@@ -0,0 +1,42 @@
+package typeoftriangle
+
+import "testing"
+
+func Test_triangleType(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Example 1",
+			args: args{nums: []int{3, 3, 3}},
+			want: "equilateral",
+		},
+		{
+			name: "Example 2",
+			args: args{nums: []int{3, 4, 5}},
+			want: "scalene",
+		},
+		{
+			name: "my test 1",
+			args: args{nums: []int{3, 3, 4}},
+			want: "isosceles",
+		},
+		{
+			name: "my test 2",
+			args: args{nums: []int{3, 3, 7}},
+			want: "none",
+		}
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := triangleType(tt.args.nums); got != tt.want {
+				t.Errorf("triangleType() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
