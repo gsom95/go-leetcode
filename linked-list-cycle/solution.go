5d4b8f91ad372bb80257106c38bfa6585ffecd72
Igor Sapozhnikov <i.d.sapozhnikov@tbank.ru>
2025-09-29 13:31:56 +0400

    add solution to the task
    https://leetcode.com/problems/linked-list-cycle/

---
+++ linked-list-cycle/solution.go
@@ -0,0 +1,20 @@
+package linkedlistcycle
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	s, f := head, head
+
+	for f != nil && f.Next != nil {
+		s = s.Next
+		f = f.Next.Next
+
+		if s == f {
+			return true
+		}
+	}
+	return false
+}
