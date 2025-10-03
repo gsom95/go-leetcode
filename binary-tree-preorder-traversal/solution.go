b8cf6db3c18dfa09ce588decbba22313cf95db47
Igor Sapozhnikov <i.d.sapozhnikov@tbank.ru>
2025-10-03 12:52:07 +0400

    add solution to the task
    https://leetcode.com/problems/binary-tree-preorder-traversal/

---
+++ binary-tree-preorder-traversal/solution.go
@@ -0,0 +1,19 @@
+package binarytreepreordertraversal
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	res := []int{root.Val}
+	res = append(res, preorderTraversal(root.Left)...)
+	res = append(res, preorderTraversal(root.Right)...)
+
+	return res
+}
