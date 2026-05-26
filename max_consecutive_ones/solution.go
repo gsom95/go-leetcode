385cc699b34affb65d01a257af4ad93c364ca814
Igor Sapozhnikov <i.d.sapozhnikov@tbank.ru>
2026-05-26 14:05:58 +0400

    update solution

--- max_consecutive_ones/solution.go
+++ max_consecutive_ones/solution.go
@@ -1,22 +1,39 @@
 package maxconsecutiveones
 
+// func findMaxConsecutiveOnes(nums []int) int {
+// 	maxOnes := 0
+// 	left := 0
+// 	zeroes := 0
+// 	for right := 0; right < len(nums); right++ {
+// 		if nums[right] == 0 {
+// 			zeroes++
+// 		}
+
+// 		for zeroes > 0 {
+// 			if nums[left] == 0 {
+// 				zeroes--
+// 			}
+// 			left++
+// 		}
+// 		maxOnes = max(maxOnes, right-left+1)
+// 	}
+
+// 	return maxOnes
+// }
+
 func findMaxConsecutiveOnes(nums []int) int {
 	maxOnes := 0
-	left := 0
-	zeroes := 0
-	for right := 0; right < len(nums); right++ {
-		if nums[right] == 0 {
-			zeroes++
+	curOnes := 0
+	for _, n := range nums {
+		if n == 1 {
+			curOnes++
+			continue
 		}
 
-		for zeroes > 0 {
-			if nums[left] == 0 {
-				zeroes--
-			}
-			left++
-		}
-		maxOnes = max(maxOnes, right-left+1)
+		maxOnes = max(maxOnes, curOnes)
+		curOnes = 0
 	}
+	maxOnes = max(maxOnes, curOnes)
 
 	return maxOnes
 }
